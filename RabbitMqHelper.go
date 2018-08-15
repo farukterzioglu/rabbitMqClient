a48c7de540bb7860015413580a609df098f056d6
Faruk Terzioğlu <faruk.terzioglu@hotmail.com>
2018-08-15 18:29:44 +0300

    implemented rabbitMqHelper

--- RabbitMqHelper.go
+++ RabbitMqHelper.go
@@ -3,10 +3,16 @@ package rabbitmq_client
 import (
 	"github.com/streadway/amqp"
 	"fmt"
+	"errors"
 )
 type IRabbitMqHelper interface{
-	GetRabbitMqConnection(hostName string,userName string ,pass string ) (*amqp.Connection, error)
-	IModel DeclareExchange(IConnection connection, string exchangeName, string exchangeType, bool durable)
+	GetRabbitMqConnection(hostName string,userName string ,
+		pass string ) (*amqp.Connection, error)
+	DeclareExchange(connection *amqp.Connection, exchangeName string ,
+		exchangeType string , durable bool ) (*amqp.Channel, error)
+	DeclareQueue(connection *amqp.Connection, exchangeName string ,
+		durable bool , queueName string , routingKey string ,
+		enablePriority bool, maxPriority int) (*amqp.Channel, error)
 }
 
 type RabbitMqHelper struct{}
@@ -15,4 +21,61 @@ func (helper *RabbitMqHelper) GetRabbitMqConnection(hostName string,userName str
 	return conn, err
 }
 
+func (helper *RabbitMqHelper) DeclareExchange(connection *amqp.Connection, exchangeName string ,
+	exchangeType string , durable bool ) (*amqp.Channel, error){
+	ch, err := connection.Channel()
+	if err != nil {
+		return nil, err
+	}
 
+	err = ch.ExchangeDeclare(
+		exchangeName,   // name
+		exchangeType, // type
+		durable,     // durable
+		  false,// auto-deleted
+		false,    // internal
+		false,    // no-wait
+		nil,      // arguments
+	)
+	return ch, err
+}
+
+func (helper *RabbitMqHelper) DeclareQueue(connection *amqp.Connection, exchangeName string ,
+	durable bool , queueName string , routingKey string ,
+	enablePriority bool, maxPriority int) (*amqp.Channel, error){
+	ch, err := connection.Channel()
+	if err != nil {
+		return nil, err
+	}
+
+	//TODO : Priority ???
+	if enablePriority == true {
+		err = errors.New("Not implemetned")
+		return nil, err
+	}
+
+	q, err := ch.QueueDeclare(
+		queueName,    // name
+		durable, // durable
+		false, // delete when usused
+		false,  // exclusive
+		false, // no-wait
+		nil,   // arguments
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	err = ch.QueueBind(
+		q.Name, // queue name
+		routingKey,     // routing key
+		exchangeName, // exchange
+		false,
+		nil,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return ch, nil
+}
