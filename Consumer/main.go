b4060cfb41f84162ed269b1b2d5deded2c7fa7e6
Faruk Terzioğlu <faruk.terzioglu@hotmail.com>
2018-08-15 19:53:49 +0300

    added a consumer

---
+++ Consumer/main.go
@@ -0,0 +1,41 @@
+package main
+
+import(
+	"github.com/farukterzioglu/rabbitmq-client"
+	"github.com/streadway/amqp"
+	"fmt"
+	"os"
+	"os/signal"
+	"log"
+)
+
+func onMessage(delivery amqp.Delivery) {
+	fmt.Printf("Got a message: %v\n", string(delivery.Body))
+}
+
+func main(){
+	println("Consumer started...")
+
+	var consumer rabbitmq_client.IRabbitMqConsumer
+	var err error
+	consumer, err = rabbitmq_client.NewRabbitMqConsumer("host", "userName", "pass",
+		"exchangeName", false, "queueName", "routingKey", false,
+		0, 50)
+	failOnError(err, "Failed to create new consumer")
+
+	err = consumer.Subscribe("test consumer", onMessage)
+	failOnError(err, "Failed to create new consumer")
+
+	quit := make(chan os.Signal)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+
+	consumer.Close()
+}
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+		panic(fmt.Sprintf("%s: %s", msg, err))
+	}
+}
